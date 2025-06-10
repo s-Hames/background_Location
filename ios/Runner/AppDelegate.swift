9bc5d6bc7a8f7bd5caaa3089a8d3c1901dea1ca9
Hames-1616 <bilquis748@gmail.com>
2025-06-10 22:44:37 +0530

    ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import flutter_background_service_ios
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Set the background task identifier
+    SwiftFlutterBackgroundServicePlugin.taskIdentifier = "com.pso.location.backgroundservice"
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
